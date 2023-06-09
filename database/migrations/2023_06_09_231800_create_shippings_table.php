744c8291473abe94edcb446a1ad8d813c199d5c7
muhamadzulfikar <muhamadzulfikar995@gmail.com>
2023-06-10 06:31:37 +0700

    Create Shipping Crud

---
+++ database/migrations/2023_06_09_231800_create_shippings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shippings', function (Blueprint $table) {
+            $table->id();
+            $table->string('shipping_type');
+            $table->string('partner_name');
+            $table->string('estimation_day');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shippings');
+    }
+};
