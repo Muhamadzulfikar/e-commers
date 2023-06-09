a01b76dd5ffdf26aaa4bb1e03c79d03e5774716f
muhamadzulfikar <muhamadzulfikar995@gmail.com>
2023-06-10 06:32:34 +0700

    Create Checkout Crud

---
+++ database/migrations/2023_06_09_231935_create_checkouts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number');
+            $table->string('receipt_payment');
+            $table->boolean('is_paid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
