7dd6d566cf0dc9a6f55d1d817a37a41b8547ad15
muhamadzulfikar <muhamadzulfikar995@gmail.com>
2023-06-11 06:21:29 +0700

    .

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
+    use CrudTrait;
     use HasFactory;
 
     protected $table = 'product_categories';
---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,5 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('product-category') }}"><i class="nav-icon la la-question"></i> Product categories</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('product') }}"><i class="nav-icon la la-question"></i> Products</a></li>
